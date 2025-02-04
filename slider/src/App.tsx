381b82a93812ad2501900d605dddf6c97d72506c
Awesomeze <ezeawesomeeze@gmail.com>
2025-02-04 18:22:10 +0100

    Giving images defined border and responsive fearture

--- slider/src/App.tsx
+++ slider/src/App.tsx
@@ -26,7 +26,7 @@ setcurrentImage ((prevSlide) => (prevSlide - 1 + images.length)% images.length)
     
 
 /* <div className='w-full  min-h-screen flex items-center pt-10' > */
-    <div className='w-full bg-gray-400  min-h-screen flex flex-col gap-3 p-4 items-center '> 
+    <div className='w-full md:h-screen bg-gray-400 h-screen flex flex-col gap-3 p-4 items-center '> 
     <h1 className="text-3xl font-bold  text-black">
     Project 
     </h1>
@@ -36,8 +36,8 @@ setcurrentImage ((prevSlide) => (prevSlide - 1 + images.length)% images.length)
       a sample of slider made with nods js, typscript and tailwind css
       </p>
   
-  <div className='min-h-3 overFlow-hidden '>
-    <img src={images[currentImage]} alt="slideshow" className='border-1 w-[300px] h-auto'/>
+  <div className='min-h-3 overFlow-hidden border-1 md:w-125 md:h-150  w-70 h-90 object-contain'>
+    <img src={images[currentImage]} alt="slideshow" className='  w-full h-full '/>
 
  <button onClick={next}
   className='absolute hover:bg-red-700 cursor-pointer right-4 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white px-3 py-1 rounded-full'>Next</button>;
