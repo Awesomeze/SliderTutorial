0e85bb1604420d71d22cedb0a6f5db707aad2336
Awesomeze <ezeawesomeeze@gmail.com>
2025-02-05 22:24:34 +0100

    added text and changed background color

--- slider/src/App.tsx
+++ slider/src/App.tsx
@@ -6,8 +6,8 @@ import { useState } from 'react'
 
 const App = () => {
   const [currentImage, setcurrentImage] = useState(0)
-  console.log("Images array:", images);
-  console.log("Current Image URL:", images[currentImage]); // Debugging
+  // console.log("Images array:", images);
+  // console.log("Current Image URL:", images[currentImage]); // Debugging
 
   
  
@@ -26,27 +26,28 @@ setcurrentImage ((prevSlide) => (prevSlide - 1 + images.length)% images.length)
     
 
 /* <div className='w-full  min-h-screen flex items-center pt-10' > */
-    <div className='w-full md:h-screen bg-gray-400 h-screen flex flex-col gap-3 p-4 items-center '> 
+    <div className='w-full  bg-gray-600 h-screen flex flex-col gap-3 p-4 items-center '> 
     <h1 className="text-3xl font-bold  text-black">
     Project 
     </h1>
 
      <p
-    className='text-green-800 font-medium '>
+    className='text-green-400 font-medium'>
       a sample of slider made with nods js, typscript and tailwind css
       </p>
   
   <div className='min-h-3 overFlow-hidden border-1 md:w-125 md:h-150  w-70 h-90 object-contain'>
-    <img src={images[currentImage]} alt="slideshow" className='  w-full h-full '/>
+    <img src={images[currentImage]} alt="slideshow" className='w-full h-full '/>
 
  <button onClick={next}
-  className='absolute hover:bg-red-700 cursor-pointer right-4 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white px-3 py-1 rounded-full'>Next</button>;
+  className='absolute hover:bg-red-700 cursor-pointer right-4 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white px-3 py-1 rounded-full'>Next</button>
  
   <button onClick={prev}
   className="absolute hover:bg-red-700 cursor-pointer left-4 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white px-3 py-1 rounded-full">prev</button>
-  </div>
-  </div>
-  // </div>
+ </div>
+ <p className='relative text-white '> feel free to make your own changes as seen fit.</p>
+ 
+ </div>
   )
 }
 
--- slider/src/repositories/images.tsx
+++ slider/src/repositories/images.tsx
@@ -1,14 +1,14 @@
 //import React from 'react'
 
-const images = [
-    <img src=".\slider\public\img.png" alt="" />,
-    <img src=".\slider\public\img1.png" alt="" />,
-    <img src=".\slider\public\img2.png" alt="" />,
+// const images = [
+//     <img src=".\slider\public\img.png" alt="" />,
+//     <img src=".\slider\public\img1.png" alt="" />,
+//     <img src=".\slider\public\img2.png" alt="" />,
 
 
-]
+// ]
 
-export default images
+// export default images
 
 
 
